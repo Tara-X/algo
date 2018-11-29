ece84a07fb06e21ee598e67224848837db752aba
superalsrk <sijialv@sohu-inc.com>
2018-11-29 18:18:19 +0800

    modify fm.py

---
+++ fm/fm.py
@@ -0,0 +1,46 @@
+#! -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+
+'''
+    FM训练
+'''
+class FMTrain(object):
+
+    
+    def __init__(self):
+        pass
+        
+    def train(self, train_data_path):
+        pass
+
+    
+    def train_sgd(self, train_data_path, usage_type):
+        pass
+
+
+
+    def dump_model(self):
+        pass
+
+'''
+    FM预测
+'''
+class FMPredict(object):
+
+    def __init__(self):
+        pass
+
+    def load_model(self, model_path):
+        pass
+
+    
+    def predict(self, feature_vec):
+        pass
+
+
+
+
